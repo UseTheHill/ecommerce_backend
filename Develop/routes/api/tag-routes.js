bffc2065a47b1b0aaa2beb3cc548e0297a9ee8ac
Kayte McDonough <kaytemcdonough@gmail.com>
2021-04-27 10:50:34 -0400

    udpated tag-routesmjs

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,7 +20,6 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 router.get('/:id', async (req, res) => {
@@ -50,7 +49,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
-  try {
+   try {
     const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
@@ -58,25 +57,25 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
-  try {
-    const tagData = await Tag.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!tagData[0]) {
-      res.status(404).json({ message: "No tag with this id!" });
-      return;
+    try {
+      const tagData = await Tag.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!tagData[0]) {
+        res.status(404).json({ message: "No tag with this id!" });
+        return;
+      }
+      res.status(200).json(tagData);
+    } catch (err) {
+      res.status(500).json(err);
     }
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tagData = await Tag.destroy({
